3f8cd4e06ba8437c1e4bba44a2fc19af2beccc6f
Nicolas Hachet <contact@nicolashachet.com>
2023-03-20 15:26:06 +0100

    Changed: config

--- Terraform/Pipeline-GH/1-versions.tf
+++ Terraform/Pipeline-GH/1-versions.tf
@@ -8,11 +8,11 @@
 #                                                                                                   |_|                                         
 
 terraform {
-  required_version = ">= 1.1.7"
+  required_version = ">= 1.3.2"
   # cf. https://www.terraform.io/language/settings/backends/azurerm
   backend "azurerm" {
-    resource_group_name  = "RG-AKSCluster"         # mettre ici le nom du resource group de vos ressource
-    storage_account_name = "backendterrapierrc"      # mettre le nom du compte de stockage créer dans le lab 1
+    resource_group_name  = "RG-AKSCluster-NH"         # mettre ici le nom du resource group de vos ressource
+    storage_account_name = "backendterranhac"      # mettre le nom du compte de stockage créer dans le lab 1
     container_name       = "tfstate"
     key                  = "pipeline-gh.terraform.tfstate" 
     # pour les options d'authentification au backend Azure Storage cf. https://www.terraform.io/language/settings/backends/azurerm
